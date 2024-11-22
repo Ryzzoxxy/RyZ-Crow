7c74095ca45cb388a7fe3f225f83a254528e534d
Ryzzoxxy <163017172+Ryzzoxxy@users.noreply.github.com>
2024-11-22 18:27:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
         owners: '1283841469320532099', // Ici tu te met owner (Que toi pas tes meilleurs collègue...)
         funny: '1283841469320532099',  // Ici tu te met owner (Que toi pas tes meilleurs collègue...)
         color: '#6495ED', // Couleur en HEX | https://htmlcolorcodes.com/fr/selecteur-de-couleur/
-        footer: 'BF Protect
+        footer: ' [+] Tatsuya
  ', // Quand tu fais +help par exemple y'a un texte en bas de l'embed...
         maxserver: '100', // Maximum de serveur ou ton bot peut etre dedans...
         hostedBy: true, // Touche pas 
